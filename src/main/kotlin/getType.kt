75bea0cdc33eeb9f3ba1bb3e2566bbf404b367b6
Erkan Çalışkan <146715229+erkan011@users.noreply.github.com>
2024-05-21 14:30:00 +0300

    Kotlin'de getType() Nedir ve Nasıl Kullanılır?
    getType() fonksiyonu, bir nesnenin türünü (type) belirlemek için kullanılır. Bu fonksiyon, bir nesnenin sınıfını veya arayüzlerini geri döndürür.
    
    Kullanım Alanları:
    
        Tip Kontrolü: Bir nesnenin belirli bir türe ait olup olmadığını kontrol etmek için kullanılabilir. Bu, kodunuzun daha sağlam ve güvenilir olmasını sağlar.
        Dönüştürme: Bir nesneyi başka bir türe dönüştürmek için kullanılabilir.
        Yansıtma: Bir nesnenin özelliklerine ve fonksiyonlarına erişmek için kullanılabilir.
    
    Kullanım Amacı:
    
    getType() fonksiyonu, bir nesnenin türüne ilişkin bilgi edinmek için kullanılır. Bu bilgi, kodunuzun nasıl davranacağını belirlemek için kullanılabilir.
    
    Alternatifler:
    
    Kotlin'de getType() fonksiyonuna benzer işlevler sunan başka fonksiyonlar da vardır. Örneğin:
    
        is operatörü: Bir nesnenin belirli bir türe ait olup olmadığını kontrol etmek için kullanılabilir.
        as operatörü: Bir nesneyi başka bir türe dönüştürmek için kullanılabilir.
        reflect kütüphanesi: Bir nesnenin özelliklerine ve fonksiyonlarına erişmek için kullanılabilir.
    
    Hangi fonksiyonu kullanacağınız, özel gereksinimlerinize bağlıdır.

---
+++ src/main/kotlin/getType.kt
@@ -0,0 +1,25 @@
+class Person(val name: String, val age: Int)
+
+fun main() {
+    val person = Person("Erkan", 22)
+    val myString: Any = "Merhaba dünya"
+
+    println("person nesnesi: ${getTypeMessage(person)}")
+    println("myString nesnesi: ${getTypeMessage(myString)}")
+}
+
+private fun Any.getType(): Any {
+    return this::class.java
+}
+
+private fun getTypeMessage(obj: Any): String {
+    return when (obj.getType()) {
+        String::class.java -> "Bu Bir Metin Dizesidir."
+        Person::class.java -> {
+            val person = obj as Person
+            "Bu Bir Person Nesnesidir. Adı: ${person.name}, Yaşı: ${person.age}"
+        }
+
+        else -> "Bilinmeyen Tür"
+    }
+}
